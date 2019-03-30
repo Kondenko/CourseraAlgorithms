34c90a63a4e5e09f69913ec173d7b1ca4e88ead9
Vladimir Kondenko <kondenko2011@gmail.com>
2019-03-30 21:40:34 +0300

    Add basic RandomizedQueue implementation

---
+++ src/com/kondenko/week3/RandomizedQueue.java
@@ -0,0 +1,118 @@
+package com.kondenko.week3;
+
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private static class RandomItem<Item> {
+        int index;
+        Item item;
+
+        public RandomItem(int index, Item item) {
+            this.index = index;
+            this.item = item;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Item[] queue = (Item[]) new Object[2];
+
+    private int first = 0;
+
+    private int last = 0;
+
+    private int size = 0;
+
+    private boolean shouldShuffle = true;
+
+    private int shufflePosition = 0;
+
+    /**
+     * add the item.
+     *
+     * @param item
+     */
+    public void enqueue(Item item) {
+        shouldShuffle = true;
+        if (size == queue.length) {
+            resize(queue.length * 2);
+        }
+        size++;
+        queue[last++] = item;
+    }
+
+    /**
+     * remove and return a random item.
+     */
+    public Item dequeue() {
+        size--;
+        Item item = queue[first++];
+        if (size / 4 == queue.length / 2) {
+            resize(queue.length / 2);
+        }
+        if (shouldShuffle) {
+            shuffle();
+        }
+        return item;
+    }
+
+    /**
+     * return a random item (but do not remove it).
+     */
+    public Item sample() {
+        return queue[StdRandom.uniform(first, last)];
+    }
+
+    /**
+     * return the number of items on the randomized queue.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * is the randomized queue empty?.
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * return an independent iterator over items in random order.
+     */
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    private void shuffle() {
+        shouldShuffle = false;
+        shufflePosition = last;
+        StdRandom.shuffle(queue, first, last);
+    }
+
+    private void resize(int capacity) {
+        @SuppressWarnings("unchecked")
+        Item[] items = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+             items[i] = queue[(first + i) % queue.length];
+        }
+        queue = items;
+        shufflePosition -= first; // TODO test if works correctly
+        first = 0;
+        last = size;
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> rqueue = new RandomizedQueue<>();
+        for (int i = 0; i < 10; i++) {
+            rqueue.enqueue(i);
+        }
+        while (!rqueue.isEmpty()) {
+            StdOut.println(rqueue.dequeue());
+        }
+    }
+
+}
