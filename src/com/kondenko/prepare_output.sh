dafbc4c72384d2bf826cfddfecc5b888d5d650b3
Vladimir Kondenko <kondenko2011@gmail.com>
2019-03-31 16:32:43 +0300

    Add a script to remove package statements and zip the results

---
+++ src/com/kondenko/prepare_output.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OUTPUT="$1" # zip file
+INPUTS="" # java files
+for i in $(seq 2 $#) # java file arguments
+do
+    FILE=${!i} # i-th command line argument
+    if [[ $(head -n 1 $FILE ) == *"package"* ]]; # if the first line is a package name
+    then
+        tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE" # remove the package name
+    fi
+    INPUTS="$INPUTS $FILE" # add the modified file to the list of inputs
+done
+
+zip $OUTPUT $INPUTS # zip the files
