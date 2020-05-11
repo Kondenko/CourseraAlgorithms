cb5efb16d870269185721d79464e8db60b92ef1d
Vladimir Kondenko <kondenko2011@gmaill.com>
2020-05-11 15:56:40 +0300

    Update the algorithm to find the length of the shortest directed cycle

--- src/com/kondenko/part2/week1/quiz/digraphs/ShortestDirectedCycle.java
+++ src/com/kondenko/part2/week1/quiz/digraphs/ShortestDirectedCycle.java
@@ -1,38 +1,43 @@
 package com.kondenko.part2.week1.quiz.digraphs;
 
 import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.Stack;
+
+import java.util.Arrays;
 
 import static com.kondenko.Utils.println;
 
 public class ShortestDirectedCycle {
 
+	private static final int NO_CYCLE = -1;
+
 	public static void find(Digraph g) {
-		Stack<Integer>[] vertices = new Stack[g.V()];
-		for (int i = 0; i < vertices.length; i++) {
-			vertices[i] = new Stack<>();
-		}
-		boolean[] marked = new boolean[g.V()];
+		int shortestCycleLength = dfs(g);
+		println("\nThe shortest cycle in G has length of %d", shortestCycleLength);
 	}
 
-/*
-	private static void dfs(Digraph g, boolean[] marked, Stack<Integer>[] vertices, int root, int dfsRoot, int current, int depth) throws ExecutionControl.NotImplementedException {
-		int lastVisitedVertex = dfs();
-		boolean isCycle = dfsRoot == lastVisitedVertex;
-		if (isCycle) {
-			vertices
+	private static int dfs(Digraph g) {
+		int[] graphDepths = new int[g.V()];
+		Arrays.fill(graphDepths, NO_CYCLE);
+		for (int i = 0; i < g.V(); i++) {
+			int[] depth = new int[g.V()];
+			Arrays.fill(depth, NO_CYCLE);
+			dfs(g, new boolean[g.V()][g.V()], i, i, depth);
+			graphDepths[i] = depth[i];
 		}
+		int minCycleDepth = NO_CYCLE;
+		for (int depth : graphDepths) {
+			if (minCycleDepth == NO_CYCLE || (depth != NO_CYCLE && depth < minCycleDepth)) {
+				minCycleDepth = depth;
+			}
+		}
+		return minCycleDepth;
 	}
-*/
 
 	private static void dfs(Digraph g, boolean[][] marked, int root, int current, int[] depth) {
-		println("dfs(s = %d, depth = %d)", current, depth[current]);
-		for (int v : g.adj(current)) {
+		Iterable<Integer> adj = g.adj(current);
+		for (int v : adj) {
 			if (!marked[current][v]) {
-				if (current == root) {
-					depth[current] = 0;
-					marked[current][v] = true;
-				}
+				marked[current][v] = true;
 				int newDepth = depth[current] + 1;
 				depth[v] = newDepth;
 				dfs(g, marked, root, v, depth);
@@ -49,27 +54,7 @@ public class ShortestDirectedCycle {
 		g.addEdge(3, 5);
 		g.addEdge(3, 4);
 		g.addEdge(4, 1);
-		// println(g.toString());
-		int root = 1;
-		dfs(g, new boolean[g.V()][g.V()], root, root, new int[g.V()]);
-	}
-
-	static class Result {
-		int depth;
-		int lastVisitedVertex;
-
-		public Result(int depth, int lastVisitedVertex) {
-			this.depth = depth;
-			this.lastVisitedVertex = lastVisitedVertex;
-		}
-
-		@Override
-		public String toString() {
-			return "Result{" +
-					"depth=" + depth +
-					", lastVisitedVertex=" + lastVisitedVertex +
-					'}';
-		}
+		ShortestDirectedCycle.find(g);
 	}
 
 }
